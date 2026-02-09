f4d6b2f3c0b279c0442497761f6607d77bddfa48
ayush <ayushvadodariya12@gmail.com>
2026-02-09 17:51:14 +0530

    Remove map from contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -85,21 +85,7 @@ export default function ContactPage() {
 
         </div>
 
-        {/* Map Embed */}
-        <div className="w-full bg-white p-2 rounded-xl shadow-lg border border-gray-200">
-           <div className="rounded-lg overflow-hidden h-96 w-full">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.2730620892913!2d73.84305887609257!3d18.65172288246738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c78162238383%3A0x6b49911956555122!2sBharat%20Complex!5e0!3m2!1sen!2sin!4v1709280000000!5m2!1sen!2sin"
-              width="100%"
-              height="100%"
-              style={{ border: 0 }}
-              allowFullScreen={true}
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Ohio Enterprise Location - Bharat Complex, MIDC Bhosari"
-            ></iframe>
-           </div>
-        </div>
+
       </div>
     </div>
   );
